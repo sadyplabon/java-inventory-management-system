6353485a95a724d72b4e5db0119aecba151f4a16
sadyplabon <plabon.pen@gmail.com>
2024-01-15 14:51:03 +0600

    Create the project

---
+++ src/main/java/com/sadyplabon/inventorymanagementsystem/InventoryManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sadyplabon.inventorymanagementsystem;
+
+/**
+ *
+ * @author plabo
+ */
+public class InventoryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
